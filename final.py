8fbcc67080162014eb98a0d66095cad14bd478bf
--unset <olara@csumb.edu>
2021-12-03 23:50:55 -0800

    created map view

--- final.py
+++ final.py
@@ -9,6 +9,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
+from geopy.geocoders import Nominatim
+
 # create an instance of Flask
 app = Flask(__name__) 
 bootstrap = Bootstrap(app)
@@ -22,11 +24,14 @@ key1 = '2751705df8c40b34efbd798cf7bdf9b6'
 key2 = '0d1d9d2ae255bb02b8d7fb4c90879192'
 keys = [key1,key2] #two API keys 
 
+API_KEY = 'AIzaSyBliiCLzc0cqvJ-uqc6yf9OFrf4jg0Oyk0'
+
+
 class Location(FlaskForm):
     name = StringField('City', validators=[DataRequired()])
 
 @app.route('/',methods=('GET', 'POST'))
-def hello():
+def hello():        
     form = Location()
     city = "United States"
     if form.validate_on_submit():
@@ -38,6 +43,7 @@ def hello():
         loc = requests.get('http://ip-api.com/json/') #documentation here: https://ip-api.com/docs
         loc_data = loc.json()
         city_weather = loc_data['city']
+        
         if city=="United States": #default
             r = requests.get('https://gnews.io/api/v4/top-headlines?lang=en&topic=nation&country=us&token='+api_key+'')
             url = 'http://api.openweathermap.org/data/2.5/weather?q='+city_weather+'&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
@@ -47,10 +53,45 @@ def hello():
             r = requests.get('https://gnews.io/api/v4/search?q='+city+'&lang=en&country=us&token='+api_key+'')
             w_data = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
         data = r.json()
-        
+
+        longitude = getLong(city)
+        latitude = getLat(city)
+        print(latitude)
+        print(longitude)
     except:
         output = 'failed'
-    return render_template('template.html',data=data,form=form,cit=city,loc_data=loc_data,w_data=w_data)
+    return render_template('template.html',data=data,form=form,cit=city,loc_data=loc_data,w_data=w_data,longitude=longitude,latitude = latitude)
+
+def getLong(address):
+    params = {
+        'key': API_KEY,
+        'address': address.replace(' ', '+')
+    }
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    if data['status'] == 'OK':
+        result = data['results'][0]
+        location = result['geometry']['location']
+        return location['lng']
+    else:
+        return
+        
+def getLat(address):
+    params = {
+        'key': API_KEY,
+        'address': address.replace(' ', '+')
+    }
+
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    if data['status'] == 'OK':
+        result = data['results'][0]
+        location = result['geometry']['location']
+        return location['lat']
+    else:
+        return
 
 #<center>
 #<div>
